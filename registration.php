6f94d58d1bdd7f76623d9893172808e45b9d9d41
mgraves236 <m.graves236@gmail.com>
2021-08-12 10:24:19 +0200

    .

--- registration.php
+++ registration.php
@@ -6,7 +6,7 @@
 require_once 'php/menu.php'; ?>
 <main>
     <iframe  src="https://docs.google.com/forms/d/e/1FAIpQLSelhg_1xBlC3WYMIFah8AnijO8BaKnjc8JVnh6PUq4eifDTWA/viewform?embedded=true"
-             width="640" height="2090" frameborder="0" marginheight="0" marginwidth="0"><article>Loading...</article></iframe>
+             width="80%" height="2090" frameborder="0" marginheight="0" marginwidth="0"><article>Loading...</article></iframe>
 </main>
 <?php require_once "php/side-bar.php"; ?>
 </body>
