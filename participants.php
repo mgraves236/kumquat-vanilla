13517685824cbb3f453fd95a73cfa4e3888d518e
mgraves236 <m.graves236@gmail.com>
2021-08-19 20:44:59 +0200

    .

--- participants.php
+++ participants.php
@@ -6,15 +6,41 @@
 require_once 'php/menu.php'; ?>
 <main>
     <article>
-        <h1>For participants</h1>
-        <h2>Abstracts</h2>
+        <h1>Abstracts</h1>
         <p>If you've already registered, send us your abstract using <a href="dcmnt/abstract.tex"
                                                                         download="kumquat_abstract.tex"><i
                         class="far fa-file-alt"></i> the template</a>.
             Then send an e-mail to kumquat@pwr.edu.pl with your abstract in a tex file. If you have any questions,
             please
             feel free to contact us!</p>
+    </article>
+    <article>
+        <h1>FAQ</h1>
+        <div class="question"> What is the cost of attending the conference?</div>
+        The conference is entirely free of charge.
 
+        <div class="question">Where will the conference be held?</div>
+            The event is fully online and will be hosted using Zoom. The speakers will be provided with meeting links
+            via the contact emails provided at registration. Presentations and guest lectures will be streamed on
+            YouTube and Twitch as well.
+
+        <div class="question"> Can I speak at the conference?</div>
+            Yes, but it requires registration. Applications and abstracts are accepted until August 29. The
+            registration form and abstract template are available on our website.
+
+        <div class="question">How much time do I have for my presentation?</div>
+            Your presentation alone should last a maximum of 12 minutes. This leaves 3 minutes for questions.
+
+        <div class="question">When will I receive confirmation that I am registered?</div>
+            We kindly ask for your patience. Confirmations will be sent to participants with a slight delay.
+
+        <div class="question">Does registration guarantee a place at the conference?</div>
+            Your participation will be decided no later than September 1. Due to the fact that this is the first
+            edition of the event, it is difficult to estimate the exact interest, which implies that some modifications
+            may be needed. We thank you in advance for your understanding.
+        </div>
+        <div class="question">Do I need to register if I choose not to prepare a presentation or poster?</div>
+        No, only the speakers are required to register.
     </article>
 </main>
 <?php require_once "php/side-bar.php"; ?>
